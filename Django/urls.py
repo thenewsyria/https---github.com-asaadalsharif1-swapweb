bc1b8eb10af8fc00835a9580a540cce15474d4cf
thenewsyria <96256695+thenewsyria@users.noreply.github.com>
2023-07-03 22:44:59 +0300

    Update urls.py

--- Django/urls.py
+++ Django/urls.py
@@ -17,8 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.csrf import csrf_failure
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapp/', include('myapp.urls')),
+    path('csrf_failure/', csrf_failure, name='csrf_failure'),
+
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
