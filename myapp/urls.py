206bea25bb4461fe2a98d043fd164969e1657a66
thenewsyria <96256695+thenewsyria@users.noreply.github.com>
2023-07-10 15:57:10 +0300

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,6 +8,7 @@ from myapp.views import payment_gateway
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
+    path('terms_and_conditions/', views.terms_and_conditions, name='terms_and_conditions'),
     #http://localhost:8000/myapp/social-login/google/
     path('social-login/<backend>/', views.social_login, name='social-login'),
     path('social-sign-up/<backend>/', views.social_sign_up, name='social-sign-up'),
@@ -16,7 +17,11 @@ urlpatterns = [
    
     path('categories/', views.get_categories, name='get_categories'),
     path('products/', views.get_products, name='get_products'),
+    path('get_all_products/', views.get_all_products, name='get_all_products'),
+    path('upload_product/', views.upload_product, name='upload_product'),
     path('add-favorite/<int:product_id>/', views.addProductToFavorit, name='add_product_to_favorit'),
+    path('get_profile/', views.get_profile , name='get_profile'),
+    path('edit_profile/', views.edit_profile , name='edit_profile'),
     path('search_products/', views.search_products, name='search_products'),
         #http://localhost:8000/myapp/search_products/?query=music
     path('submit_support/', views.submit_support, name='submit_support'),
