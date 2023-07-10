d1a10d9529d9a5585ea87b22af9eceddd62a17c2
thenewsyria <96256695+thenewsyria@users.noreply.github.com>
2023-07-10 08:34:19 +0300

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from myapp.models import User, Categorys, Products,Support, Purchase, Payment, Message, Contract
+from myapp.models import User, Categorys, Products,Favorit,Support, Purchase, Payment, Message, Contract
 
 admin.site.register(User)
 admin.site.register(Categorys)
 admin.site.register(Products)
+admin.site.register(Favorit)
 admin.site.register(Purchase)
 admin.site.register(Payment)
 admin.site.register(Message)
